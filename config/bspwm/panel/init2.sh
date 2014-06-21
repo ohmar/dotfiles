3223e9ff9f9c1442f89dd1b6b5c5e3ec3148f5a1
Omar Sandoval <omar@sandovoid.com>
2014-06-21 15:40:24 -0700

    Properly tracked most files important files that lie in .config

---
+++ config/bspwm/panel/init2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+killall -9 conky bar
+
+FONT="-*-terminus-symbols-*-*-*-*-*-*-*-*-*-*-*-*"
+OPACITY="fff"
+FG="bbbbbb"
+# BG="1b1d1e"
+BG="1c1c1c"
+conky -c ~/.config/bspwm/panel/conkyrc | bar -g 1680x12+0 -p -B "#$OPACITY$BG" -F "#$OPACITY$FG" -f $FONT | sh
